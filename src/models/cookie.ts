6227e4088ffc979062695da1f519a8cce2d7cb62
ohjunsang <pandati@dsm.hs.kr>
2021-03-30 19:46:21 +0900

    [ADD] 사용하는 type들 정리

---
+++ src/models/cookie.ts
@@ -0,0 +1,31 @@
+export type cookie = {
+  name: string;
+  rank: string;
+  tier: number;
+  major: string;
+  position: string;
+  topping: string;
+  skill_name: string;
+  skill_description: string;
+  cool_time: number;
+  cookie_image_url: string;
+  skill_image_url: string;
+  major_image_url: string;
+};
+
+export type treasure = {
+  name: string;
+  rank: string;
+  effect: string;
+  description: string;
+  image_url: string;
+};
+
+export type deck = {
+  type: type;
+  cookie: string[];
+  treasure: string[];
+  substitution: string[];
+};
+
+export type type = 'PVP' | 'PVE' | 'RAID';
