896436d6359c04da389041f96899d8d98a12dcec
ohjunsang <pandati@dsm.hs.kr>
2021-04-01 15:02:58 +0900

    [FIX] 임시 key 추가

--- src/components/deckList/DeckList.tsx
+++ src/components/deckList/DeckList.tsx
@@ -2,14 +2,18 @@ import React, { FC } from 'react';
 import useDeckUseCase from '../../hooks/useCase/deck/useDeckUseCase';
 import { deck } from '../../models/cookie';
 import Deck from './deck';
+import Header from './header';
 
 const DeckList: FC = () => {
   const { deck, deckType, setDeckType } = useDeckUseCase();
   const renderDeck = (decks: deck[]) => {
-    return decks.map(deck => <Deck cookieList={deck.cookie} treasureList={deck.treasure} />);
+    return decks.map((deck, index) => (
+      <Deck cookieList={deck.cookie} treasureList={deck.treasure} key={index} />
+    ));
   };
   return (
     <div className='decklist__wrapper'>
+      <Header deckType={deckType} setDeckType={setDeckType} />
       <div>{renderDeck(deck)}</div>
     </div>
   );
