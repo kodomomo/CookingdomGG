c143738330b59003711445f013c73df422edd1b2
ohjunsang <pandati@dsm.hs.kr>
2021-04-05 10:57:34 +0900

    [FIX] 린트 적용

--- src/components/cookieDetail/CookieDetail.tsx
+++ src/components/cookieDetail/CookieDetail.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useEffect } from 'react'
-import useCookieUseCase from '../../hooks/useCase/cookie/useCookieUseCase'
-import { Cookie } from './cookie'
-import { Topping } from './topping'
-import { Skill } from './skill'
+import React, { FC, useEffect } from 'react';
+import useCookieUseCase from '../../hooks/useCase/cookie/useCookieUseCase';
+import { Cookie } from './cookie';
+import { Topping } from './topping';
+import { Skill } from './skill';
 
 interface Props {
   open: boolean;
@@ -10,36 +10,44 @@ interface Props {
   cookieName: string;
 }
 
-const CookieDetail: FC<Props> = ({
-  open,
-  setOpen,
-  cookieName
-}) => {
+const CookieDetail: FC<Props> = ({ open, setOpen, cookieName }) => {
   const cookieDetailClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-  }
-  useEffect(()=> {
+  };
+  useEffect(() => {
     window.addEventListener('click', () => {
-      setOpen(false)
+      setOpen(false);
     });
   }, []);
 
   const { cookie, setName } = useCookieUseCase();
 
-  useEffect(()=> {
-    setName(cookieName)
+  useEffect(() => {
+    setName(cookieName);
   }, [cookieName]);
 
   return (
-  <div className={`cookiedetail ${open ? "cookiedetail--open" : ""}`} onClick={cookieDetailClickHandler}>
-    <div className='cookiedetail__wrapper'>
-      <Cookie name={cookie.name} position={cookie.position} rank={cookie.rank} cookieImageUri={cookie.cookie_image_url}/>
-      <Topping topping={""} toppingImageUri={""}/>
-      <Skill skillName={cookie.skill_name} coolTime={cookie.cool_time} skillImageUri={cookie.skill_image_url} 
-        skillDescription={cookie.skill_description}/>
+    <div
+      className={`cookiedetail ${open ? 'cookiedetail--open' : ''}`}
+      onClick={cookieDetailClickHandler}
+    >
+      <div className='cookiedetail__wrapper'>
+        <Cookie
+          name={cookie.name}
+          position={cookie.position}
+          rank={cookie.rank}
+          cookieImageUri={cookie.cookie_image_url}
+        />
+        <Topping topping={''} toppingImageUri={''} />
+        <Skill
+          skillName={cookie.skill_name}
+          coolTime={cookie.cool_time}
+          skillImageUri={cookie.skill_image_url}
+          skillDescription={cookie.skill_description}
+        />
+      </div>
     </div>
-  </div>
   );
 };
 
