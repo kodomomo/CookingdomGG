02f4d4748bb8b13b826bb938d85a4a4da59d8068
LeagueLugas <leaguelugas@gmail.com>
2021-04-06 19:10:11 +0900

    [UPDATE]

--- src/components/cookieList/CookieList.tsx
+++ src/components/cookieList/CookieList.tsx
@@ -6,56 +6,72 @@ import { cookie } from '../../models/cookie';
 import config from '../../config';
 
 interface Props {
-    openDetailCookie: (value: string) => void;
+  openDetailCookie: (value: string) => void;
 }
 
 const CookieList: FC<Props> = ({ openDetailCookie }) => {
+  const {
+    cookieList,
+    searchType,
+    setSearchType,
+    tier,
+    setTier,
+    rank,
+    setRank,
+    position,
+    setPosition,
+    major,
+    setMajor,
+  } = useCookieListUseCase();
 
-    const {
-        cookieList,
-        searchType, setSearchType,
-        tier, setTier,
-        rank, setRank,
-        position, setPosition,
-        major, setMajor
-    } = useCookieListUseCase();
+  const getCookieListClickHandler = (cookieName: string) => (
+    e: React.MouseEvent<HTMLDivElement>,
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
+    openDetailCookie(cookieName);
+  };
 
-    const getCookieListClickHandler = (cookieName: string) => (e:React.MouseEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        openDetailCookie(cookieName);
-      };
-
-    const renderCookieList = (cookies: cookie[]) => {
-        let i = 0;
-        return cookies.map((cookie, index) => (
-            <Zoom in={true} style={{
-                    transitionDelay: `${i++ * 15}ms`,
-                    height: '100px',
-                    borderRadius: '15px'
-                }}>
-                <div className="cookielist__content__list__paper" onClick={getCookieListClickHandler(cookie.name)}>
-                    <img src={`${config.apiHost}/image/${cookie.cookie_image_url}`} />
-                </div>
-            </Zoom>
-        ));
-    }
-
-    return (
-        <div id="cookielist__wrapper">
-            <Header 
-                searchType={searchType} setSearchType={setSearchType}
-                tier={tier} setTier={setTier}
-                rank={rank} setRank={setRank}
-                position={position} setPosition={setPosition}
-                major={major} setMajor={setMajor} />
-            <div className="cookielist__content">
-                <div className="cookielist__content__list">
-                    {renderCookieList(cookieList)}
-                </div>
-            </div>
+  const renderCookieList = (cookies: cookie[]) => {
+    let i = 0;
+    return cookies.map((cookie, index) => (
+      <Zoom
+        in={true}
+        style={{
+          transitionDelay: `${i++ * 15}ms`,
+          height: '100px',
+          borderRadius: '15px',
+        }}
+      >
+        <div
+          className='cookielist__content__list__paper'
+          onClick={getCookieListClickHandler(cookie.name)}
+        >
+          <img src={`${config.apiHost}/image/${cookie.cookie_image_url}`} />
         </div>
-    );
+      </Zoom>
+    ));
+  };
+
+  return (
+    <div id='cookielist__wrapper'>
+      <Header
+        searchType={searchType}
+        setSearchType={setSearchType}
+        tier={tier}
+        setTier={setTier}
+        rank={rank}
+        setRank={setRank}
+        position={position}
+        setPosition={setPosition}
+        major={major}
+        setMajor={setMajor}
+      />
+      <div className='cookielist__content'>
+        <div className='cookielist__content__list'>{renderCookieList(cookieList)}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CookieList;
